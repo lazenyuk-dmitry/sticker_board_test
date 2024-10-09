b8595b1290f74b0d9d913f9d05582c6d5a401c71
Dmitry Lazenyuk <lazenyukdima@gmail.com>
2024-10-09 10:11:38 +0500

    Create base layouts.

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtLayout>
+    <v-app>
+      <v-app-bar>
+        <v-spacer/>
+        <v-btn icon="mdi-pencil" />
+      </v-app-bar>
+      <v-main>
+        <NuxtPage />
+      </v-main>
+    </v-app>
+  </NuxtLayout>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="n in 24"
+        :key="n"
+        cols="4"
+      >
+        <v-card height="200"></v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
